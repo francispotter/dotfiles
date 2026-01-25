055d6e7a3f9b3a7b4ebd2e15249fa49c8b28a1fc
Francis Potter <git@fpotter.com>
2026-01-24 19:13:24 -0800

    Add MacOS support and set zsh as default shell

--- install.sh
+++ install.sh
@@ -3,9 +3,20 @@
 
 # Install required packages on Debian-based Linux
 if [ -f /etc/debian_version ]; then
-    echo "Detected Debian-based Linux. Installing required packages..."
+    echo "Installing for Debian..."
     sudo apt-get update
     sudo  DEBIAN_FRONTEND=noninteractive apt-get install -y emacs zsh tmux
+fi
+
+# Install required packages on MacOS
+if [ "$(uname)" = "Darwin" ]; then
+    echo "Installing for MacOS..."
+    # Check if Homebrew is installed
+    if ! command -v brew &> /dev/null; then
+        echo "Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    brew install emacs zsh tmux
 fi
 
 rm -rf ~/.zdotdir ~/.emacs.d ~/.dotfiles ~/.editrc ~/.tmux.conf
@@ -15,3 +26,13 @@ ln -s ~/.dotfiles/.emacs.d ~/.emacs.d
 ln -s ~/.dotfiles/.editrc ~/.editrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 echo "export ZDOTDIR=$HOME/.zdotdir" > $HOME/.zshenv
+
+if [ -f /etc/debian_version ]; then
+  sudo usermod --shell /bin/zsh $(whoami)
+fi
+
+if [ "$(uname)" = "Darwin" ]; then
+  sudo chsh -s /bin/zsh $(whoami)
+fi
+
+exec zsh
