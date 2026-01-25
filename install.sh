e4a00275aa9cbef1ed4bf111b94bc7f3798bd503
Francis Potter <git@fpotter.com>
2026-01-24 17:39:44 -0800

    Add automatic package installation for Debian-based systems

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 # Installs all of DotFiles - Francis Potter on any *nix system
 # DESTRUCTIVE!
+
+# Install required packages on Debian-based Linux
+if [ -f /etc/debian_version ]; then
+    echo "Detected Debian-based Linux. Installing required packages..."
+    sudo apt-get update
+    sudo apt-get install -y emacs zsh tmux
+fi
+
 rm -rf ~/.zdotdir ~/.emacs.d ~/.dotfiles ~/.editrc ~/.tmux.conf
 git clone https://gitlab.com/francispotter/dotfiles.git ~/.dotfiles
 ln -s ~/.dotfiles/.zdotdir ~/.zdotdir
